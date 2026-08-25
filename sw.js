37d64e075ff979c7f64af4bdf5409297b1c3bd76
henrysemple1970-ui <henrysemple1970@gmail.com>
2026-08-25 18:02:31 +0100

    Improve stable MOT Evidence sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mot-evidence-guided-wizard-v7";
+const CACHE="mot-evidence-guided-wizard-v8";
 const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -8,3 +8,4 @@ self.addEventListener("fetch",e=>{
     const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
   }).catch(()=>caches.match(e.request)));
 });
+
