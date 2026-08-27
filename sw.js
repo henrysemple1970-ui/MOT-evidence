bed2d8d316fe26d75b061822659ea9cfeeec567d
henrysemple1970-ui <henrysemple1970@gmail.com>
2026-08-27 12:45:33 +0100

    Refresh cache for restored Google sign-in

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mot-evidence-guided-wizard-v8-1-3";
+const CACHE="mot-evidence-guided-wizard-v8-1-4";
 const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
